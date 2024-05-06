b557caa691067e1ccbd4f413978420b424270eb5
Samundeeswari Velusamy <sam.velusamy36@gmail.com>
2024-05-06 16:46:24 -0400

    Reading a Delimited file

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+//        ScanDelimitedFile obj = new ScanDelimitedFile();
+//        obj.readDelimitedFile();
+
+        MyRunner obj = new MyRunner();
+        obj.myRunner();
+        }
+    }
